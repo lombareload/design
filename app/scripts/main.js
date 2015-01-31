6e7eaf8dc3192edec1208cf8914862a30642430c
Fabián López <fabian.lopez@prodigious.com>
2015-01-31 17:41:25 -0500

    created galeria section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,29 @@
+'use strict';
 $(function(){
     /* toggle header */
-    var lastWasHome = true;
-    var homeParent = $("[href=#home]").parent();
-    var header = $(".header");
-    $("body").on("activate.bs.scrollspy", function(asd){
+    var lastWasHome = false;
+    var homeParent = $('[href=#home]').parent();
+    var header = $('.header');
+    $('body').on('activate.bs.scrollspy', function(){
         var parentClass = homeParent.attr('class');
-        if(parentClass){
-            header.toggleClass("visual");
+        var hasHidingClass = header.hasClass('visual');
+        if(parentClass && !hasHidingClass){
+            console.log('asd');
+            header.toggleClass('visual');
             lastWasHome = true;
-        } else if(lastWasHome){
-            header.toggleClass("visual");
+        } else if(!parentClass && hasHidingClass){
+            console.log('asd1');
+            header.toggleClass('visual');
             lastWasHome = false;
         }
     });
     /* toggle header end */
 
-    var sections = ["home", "skills", "portrait"];
-    
+    var sections = ['home', 'skills', 'portrait'];
+
     function sectionEvent(sectionID){
-        $("[href=#" + sectionID + "]").click(function(){
-            $("#" + sectionID).scroll();
+        $('[href=#' + sectionID + ']').click(function(){
+            $('#' + sectionID).scroll();
             console.log(sectionID);
         });
     }
