d42ddfc76ee115ad5a2dba51e822f430facb2057
Fabián López <fabian.lopez@prodigious.com>
2015-01-31 11:14:55 -0500

    added transitions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+    /* toggle header */
+    var lastWasHome = true;
+    var homeParent = $("[href=#home]").parent();
+    var header = $(".header");
+    $("body").on("activate.bs.scrollspy", function(asd){
+        var parentClass = homeParent.attr('class');
+        if(parentClass){
+            header.toggleClass("visual");
+            lastWasHome = true;
+        } else if(lastWasHome){
+            header.toggleClass("visual");
+            lastWasHome = false;
+        }
+    });
+    /* toggle header end */
+
+    var sections = ["home", "skills", "portrait"];
+    
+    function sectionEvent(sectionID){
+        $("[href=#" + sectionID + "]").click(function(){
+            $("#" + sectionID).scroll();
+            console.log(sectionID);
+        });
+    }
+
+    sections.forEach(sectionEvent);
+});
